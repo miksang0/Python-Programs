7b957ba3a317e0a428218171f2d4c279d8fe0c9f
Sujan Tamang <69522312+miksang0@users.noreply.github.com>
2025-11-18 10:29:14 -0800

    Add files via upload

---
+++ Ascending And Descending Order/Ascending And Descending.py
@@ -0,0 +1,10 @@
+numbers = [34, 1, 23, 4, 3, 12, 5, 2]
+numbers.sort(reverse=False)
+print(numbers)
+
+numbers.sort(reverse=True)
+print(numbers)
+
+words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
+descending_order = sorted(words, reverse=True)
+print("Descending_order:", descending_order)
